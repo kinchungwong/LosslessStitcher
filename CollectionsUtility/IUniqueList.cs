0439476e81ff2523531cef352906723985aa5104
kinchungwong <kinchungwong@users.noreply.github.com>
2020-09-05 04:59:24 -0700

    CollectionsUtility added UniqueList
    Also IUniqueList, ReadOnlyUniqueList.
    
    Not tested. However, the code is ported from the original project, which means it is already well-tested, unless typos were made when the code is copied.

---
+++ CollectionsUtility/IUniqueList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionsUtility
+{
+    public interface IUniqueList<T>
+        : IReadOnlyList<T>
+        , IEnumerable<(int Index, T Item)>
+    {
+        int IndexOf(T t);
+
+        T ItemAt(int index);
+    }
+}
---
+++ CollectionsUtility/ReadOnlyUniqueList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsUtility
+{
+    public class ReadOnlyUniqueList<T>
+        : IUniqueList<T>
+    {
+        private IUniqueList<T> _target;
+
+        public int Count => _target.Count;
+
+        public T this[int index] => ItemAt(index);
+
+        public ReadOnlyUniqueList(IUniqueList<T> target)
+        {
+            if (target is null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+            _target = target;
+        }
+
+        public int IndexOf(T t) => _target.IndexOf(t);
+
+        public T ItemAt(int index) => _target.ItemAt(index);
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)_target).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_target).GetEnumerator();
+
+        IEnumerator<(int Index, T Item)> IEnumerable<(int Index, T Item)>.GetEnumerator() =>
+            ((IEnumerable<(int Index, T Item)>)_target).GetEnumerator();
+    }
+}
